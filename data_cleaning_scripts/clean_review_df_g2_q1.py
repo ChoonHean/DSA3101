6168228f735e86db6ac102c662a857d078ad053e
Dillon Chong <124565587+dillonchong01@users.noreply.github.com>
2025-03-09 13:57:30 +0800

    Update clean_review_df_g2_q1.py
    Added sentiment analysis using Vader Pretrained model to obtain sentiment score (1 - 5)
    Converted timestamp to year and quarter

--- data_cleaning_scripts/clean_review_df_g2_q1.py
+++ data_cleaning_scripts/clean_review_df_g2_q1.py
@@ -1,14 +1,42 @@
-# load dataset
 import pandas as pd
-df_review = pd.read_json("Amazon_Fashion.jsonl", lines=True)
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# select columns
-useful_columns = ['title', 'text', 'parent_asin', 'timestamp', 'helpful_vote', 'verified_purchase']
+# Load dataset and elect columns
+df_review = pd.read_json("C:\DSA3101\Amazon_Fashion.jsonl", lines=True, nrows = 1000)
+useful_columns = ['rating', 'title', 'text', 'parent_asin', 'timestamp', 'verified_purchase']
 df_review = df_review[useful_columns]
 
-# drop rows where verified_purchase is False
-df_review = df_review[df_review['verified_purchase'] == True]
+# Drop rows where verified_purchase is False
+df_review = df_review[df_review['verified_purchase']]
 
-# drop NAs and duplications
+# Split timestamp into year and quarter
+df_review['year'] = df_review['timestamp'].dt.year
+df_review['quarter'] = df_review['timestamp'].dt.quarter
+
+# Combine title and text into a new column
+df_review['review'] = df_review['title'] + " " + df_review['text']
+
+# Drop NAs, duplications and unnecessary columns 
 df_review.dropna(subset=['parent_asin'], inplace=True)
 df_review = df_review.drop_duplicates(subset=['title', 'text', 'parent_asin'])
+df_review.drop(columns=['verified_purchase', 'timestamp', 'title', 'text'], inplace=True)
+
+
+# Sentiment Analysis Score (From 1 - 5) based on Review
+# Remove links in review
+def remove_url(text):
+    url_pattern = r'http\S+|www\S+|https\S+'
+    return re.sub(url_pattern, '', text)
+df_review['review'] = df_review['review'].apply(remove_url)
+
+# # Use Vader Sentiment pretrained model to calculate a sentiment score from 1 to 5
+analyzer = SentimentIntensityAnalyzer()
+def get_vader_sentiment(text):
+    sentiment = analyzer.polarity_scores(text)
+    return round(sentiment['compound'] * 2 + 3, 1)  # Adjust sentiment score range 0 - 5, and round to 1 decimal place
+
+# Apply the function to the 'review' column
+df_review['sentiment_score'] = df_review['review'].apply(get_vader_sentiment)
+
+print(df_review.head())
