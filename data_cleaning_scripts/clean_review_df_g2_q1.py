fdff60e5ff2491d456df35089f72f1c4bae521da
jl5045 <jinluichuah@gmail.com>
2025-03-16 23:36:04 +0800

    Cleaned the code and added docstrings for clean_review_df_g2_q1.py

--- data_cleaning_scripts/clean_review_df_g2_q1.py
+++ data_cleaning_scripts/clean_review_df_g2_q1.py
@@ -2,44 +2,54 @@ import pandas as pd
 import re
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# Load dataset and select columns
-df_review = pd.read_json("../dataset/raw_data/Amazon_Fashion.jsonl", lines=True)
-useful_columns = ['rating', 'title', 'text', 'parent_asin', 'timestamp', 'verified_purchase']
-df_review = df_review[useful_columns]
+def remove_url(text: str) -> str:
+    """
+    Remove url from review text.
 
-# Drop rows where verified_purchase is False
-df_review = df_review[df_review['verified_purchase']]
-
-# Split timestamp into year and quarter
-df_review['year'] = df_review['timestamp'].dt.year
-df_review['quarter'] = df_review['timestamp'].dt.quarter
-
-# Combine title and text into a new column
-df_review['review'] = df_review['title'] + " " + df_review['text']
-
-# Drop NAs, duplications and unnecessary columns 
-df_review.dropna(subset=['parent_asin'], inplace=True)
-df_review = df_review.drop_duplicates(subset=['title', 'text', 'parent_asin'])
-df_review.drop(columns=['verified_purchase', 'timestamp', 'title', 'text'], inplace=True)
-
-
-# Sentiment Analysis Score (From 1 - 5) based on Review
-# Remove links in review
-def remove_url(text):
+    :param text: Review text
+    :return: Cleaned review text
+    """
     url_pattern = r'http\S+|www\S+|https\S+'
     return re.sub(url_pattern, '', text)
-df_review['review'] = df_review['review'].apply(remove_url)
 
-# # Use Vader Sentiment pretrained model to calculate a sentiment score from 1 to 5
-analyzer = SentimentIntensityAnalyzer()
 def get_vader_sentiment(text):
+    """
+    Computes the sentiment score of a given text using the VADER sentiment analysis model.
+
+    :param text: Review text.
+    :return: Sentiment score range from 1 to 5, round to 1 decimal place.
+    """
     sentiment = analyzer.polarity_scores(text)
-    return round(sentiment['compound'] * 2 + 3, 1)  # Adjust sentiment score range 0 - 5, and round to 1 decimal place
+    return round(sentiment['compound'] * 2 + 3, 1)
 
-# Apply the function to the 'review' column
-df_review['sentiment_score'] = df_review['review'].apply(get_vader_sentiment)
+if __name__ == "__main__":
+    # Load dataset and select columns
+    df_review = pd.read_json("../dataset/raw_data/Amazon_Fashion.jsonl", lines=True)
+    useful_columns = ['rating', 'title', 'text', 'parent_asin', 'timestamp', 'verified_purchase']
+    df_review = df_review[useful_columns]
 
-print(df_review.head())
+    # Drop rows where verified_purchase is False
+    df_review = df_review[df_review['verified_purchase']]
 
-# save the data into csv
-df_review.to_csv("../dataset/cleaned_data/final_cleaned_review.csv", index=False)
+    # Split timestamp into year and quarter
+    df_review['year'] = df_review['timestamp'].dt.year
+    df_review['quarter'] = df_review['timestamp'].dt.quarter
+
+    # Combine title and text into a new column
+    df_review['review'] = df_review['title'] + " " + df_review['text']
+
+    # Drop NAs, duplications and unnecessary columns
+    df_review.dropna(subset=['parent_asin'], inplace=True)
+    df_review = df_review.drop_duplicates(subset=['title', 'text', 'parent_asin'])
+    df_review.drop(columns=['verified_purchase', 'timestamp', 'title', 'text'], inplace=True)
+
+    # Remove links in review
+    df_review['review'] = df_review['review'].apply(remove_url)
+
+    # Use Vader Sentiment pretrained model to calculate a sentiment score from 1 to 5
+    analyzer = SentimentIntensityAnalyzer()
+    df_review['sentiment_score'] = df_review['review'].apply(get_vader_sentiment)
+    print(df_review.head())
+
+    # Save the data into csv
+    df_review.to_csv("../dataset/cleaned_data/final_cleaned_review.csv", index=False)
