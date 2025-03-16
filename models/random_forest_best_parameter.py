af0667b9e53f1434a8ab72abb3f94870615b0b88
jl5045 <jinluichuah@gmail.com>
2025-03-17 00:04:45 +0800

    Cleaned the code and added docstrings for random_forest_best_parameter.py

--- models/random_forest_best_parameter.py
+++ models/random_forest_best_parameter.py
@@ -2,39 +2,30 @@ from sklearn.model_selection import GridSearchCV
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from random_forest_train import preprocess, split_train_test
 
-df = pd.read_csv("../dataset/cleaned_data/final_combined_dataset.csv")
+if __name__ == '__main__':
+    # load dataset
+    df = pd.read_csv("../dataset/cleaned_data/final_combined_dataset.csv")
 
-df = df.sort_values(by=["cluster_label", "year", "quarter"])
-df["num_sales_lag_1Q"] = df.groupby("cluster_label")["num_sales"].shift(1)
-df["num_sales_lag_2Q"] = df.groupby("cluster_label")["num_sales"].shift(2)
-df["num_sales_lag_4Q"] = df.groupby("cluster_label")["num_sales"].shift(4)
+    # preprocess data for splitting
+    df = preprocess(df=df)
 
-df = pd.get_dummies(df, columns=["cluster_label"], drop_first=True)
-df["num_sales"] = np.log1p(df["num_sales"])
+    # split train-test data
+    X_train, X_test, y_train, y_test = split_train_test(df=df, target_col="num_sales", split_year=2021)
 
-df.fillna(0, inplace=True)
+    # initialise param_grid for grid search
+    param_grid = {
+        "n_estimators": [100, 300, 500],
+        "max_depth": [10, 20, None],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4]
+    }
 
-X = df.drop(columns=["num_sales"])
-y = df["num_sales"]
+    # conduct grid search for hyperparameter tuning of random forest regressor
+    rf = RandomForestRegressor(random_state=42)
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv= 5, scoring="neg_root_mean_squared_error", n_jobs=-1)
+    grid_search.fit(X_train, y_train)
 
-split_year = 2021
-X_train = X[df["year"] < split_year]
-X_test = X[df["year"] >= split_year]
-y_train = y[df["year"] < split_year]
-y_test = y[df["year"] >= split_year]
-
-param_grid = {
-    "n_estimators": [100, 300, 500],
-    "max_depth": [10, 20, None],
-    "min_samples_split": [2, 5, 10],
-    "min_samples_leaf": [1, 2, 4]
-}
-
-rf = RandomForestRegressor(random_state=42)
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv= 5, scoring="neg_root_mean_squared_error", n_jobs=-1)
-grid_search.fit(X_train, y_train)
-
-print(f"Best Parameters: {grid_search.best_params_}")
-
-# Best Parameters: {'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 500}
+    print(f"Best Parameters: {grid_search.best_params_}")
+    # Best Parameters: {'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 500}
