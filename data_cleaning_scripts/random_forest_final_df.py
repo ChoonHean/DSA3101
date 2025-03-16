e9818ef889b093f3060519fe9128c5dc295b34ce
Yue Xinyu <e1136979@u.nus.edu>
2025-03-16 15:19:02 +0800

    Add comments for easier refering

--- data_cleaning_scripts/random_forest_final_df.py
+++ data_cleaning_scripts/random_forest_final_df.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from itertools import product
 
+# Load cleaned metadata and review datasets
 df_final_metadata = pd.read_csv("../dataset/cleaned_data/final_cleaned_metadata.csv")
 df_final_review = pd.read_csv("../dataset/cleaned_data/final_cleaned_review.csv")
 
 df_combined = df_final_metadata.merge(df_final_review, on=["parent_asin"], how="left")
 
+# Find min year,quarter and max year,quarter to build a full date table
 min_year = int(df_combined["year"].min())
 max_year = int(df_combined["year"].max())
-
 min_quarter = int(df_combined.loc[df_combined["year"] == min_year, "quarter"].min())
 max_quarter = int(df_combined.loc[df_combined["year"] == max_year, "quarter"].max())
 
@@ -17,7 +18,6 @@ year, quarter = min_year, min_quarter
 
 while (year < max_year) or (year == max_year and quarter <= max_quarter):
     date_combinations.append((year, quarter))
-
     if quarter == 4:
         year += 1
         quarter = 1
@@ -30,6 +30,7 @@ full_date_table = pd.DataFrame(product(df_combined["cluster_label"].unique(), da
 full_date_table[["year", "quarter"]] = pd.DataFrame(full_date_table["date"].tolist(), index=full_date_table.index)
 full_date_table.drop(columns=["date"], inplace=True)
 
+# Merge the full date table with aggregated data to ensure all (year, quarter) combinations exist
 df_aggregated = df_combined.groupby(["cluster_label", "year", "quarter"]).agg({
     "review": "count",
     "sentiment_score": "mean",
@@ -46,5 +47,6 @@ df_final = df_final.sort_values(by=["cluster_label", "year", "quarter"])
 
 print(df_final.head())
 
+# save the final data into csv
 df_final.to_csv("../dataset/cleaned_data/final_combined_dataset.csv", index=False)
 
