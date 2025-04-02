4e382c99bd8339d03ea4b97931faf9b4de22bcb5
Yue Xinyu <e1136979@u.nus.edu>
2025-04-02 20:15:34 +0800

    Update high_demand_scenario.py
    remove unnecessary comment

--- group_b/inventory/high_demand_scenario.py
+++ group_b/inventory/high_demand_scenario.py
@@ -24,10 +24,6 @@ naive_results_df = apply_naive_inventory_strategy(demand_df, stock_df)
 
 print(naive_results_df.head())
 
-# Save results if needed
-# naive_results_df.to_csv("../raw_data/high_demand_naive_inventory_results.csv", index=False)
-
-
 # Apply our algo
 service_level = 0.95
 z_score = norm.ppf(service_level)
@@ -35,10 +31,6 @@ z_score = norm.ppf(service_level)
 algo_result_df = get_quarterly_restock(demand_df, stock_df, z_score)
 
 print(algo_result_df.head())
-
-# Save results if needed
-# algo_result_df.to_csv("../raw_data/high_demand_algo_result.csv", index=False)
-
 
 # Make comparison
 comparison = compare_inventory_models(algo_result_df, naive_results_df, stock_df)
